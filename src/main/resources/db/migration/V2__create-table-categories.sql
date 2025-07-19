4c04d803d61818168f90fa3758c4148457a548f7
samanthamaiaduarte <sam.maiaduarte@outlook.com>
2025-07-19 18:32:32 -0300

    feat: adjusting description - removing NOT NULL

--- src/main/resources/db/migration/V2__create-table-categories.sql
+++ src/main/resources/db/migration/V2__create-table-categories.sql
@@ -3,5 +3,5 @@ CREATE EXTENSION IF NOT EXISTS "pgcrypto";
 CREATE TABLE IF NOT EXISTS categories(
     id UUID DEFAULT gen_random_uuid() PRIMARY KEY,
     name VARCHAR(100) NOT NULL UNIQUE,
-    description VARCHAR(200) NOT NULL
+    description VARCHAR(200)
 );
--- src/main/resources/db/migration/V3__create-table-products.sql
+++ src/main/resources/db/migration/V3__create-table-products.sql
@@ -3,7 +3,7 @@ CREATE EXTENSION IF NOT EXISTS "pgcrypto";
 CREATE TABLE IF NOT EXISTS products(
     id UUID DEFAULT gen_random_uuid() PRIMARY KEY,
     name VARCHAR(100) NOT NULL,
-    description VARCHAR(200) NOT NULL,
+    description VARCHAR(200),
     price DECIMAL(10,2) NOT NULL,
     created_at TIMESTAMP NOT NULL,
     image_url VARCHAR,
