4d87973c177928fd25f497c45f47a97d08c01930
samanthamaiaduarte <sam.maiaduarte@outlook.com>
2025-07-19 15:26:25 -0300

    feat: add migrations - tables: users, categories, products

---
+++ src/main/resources/db/migration/V2__create-table-categories.sql
@@ -0,0 +1,7 @@
+CREATE EXTENSION IF NOT EXISTS "pgcrypto";
+
+CREATE TABLE IF NOT EXISTS categories(
+    id UUID DEFAULT gen_random_uuid() PRIMARY KEY,
+    name VARCHAR(100) NOT NULL UNIQUE,
+    description VARCHAR(200) NOT NULL
+);
---
+++ src/main/resources/db/migration/V3__create-table-products.sql
@@ -0,0 +1,12 @@
+CREATE EXTENSION IF NOT EXISTS "pgcrypto";
+
+CREATE TABLE IF NOT EXISTS products(
+    id UUID DEFAULT gen_random_uuid() PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    description VARCHAR(200) NOT NULL,
+    price DECIMAL(10,2) NOT NULL,
+    created_at TIMESTAMP NOT NULL,
+    image_url VARCHAR,
+    category_id UUID NOT NULL,
+    FOREIGN KEY (category_id) REFERENCES categories(id)
+);
