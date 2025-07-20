2f9b5d8a7b0bd773c3dc31469460c8b733939389
samanthamaiaduarte <sam.maiaduarte@outlook.com>
2025-07-20 18:35:59 -0300

    feat: Adjusting Size error message

--- src/main/java/com/samanthamaiaduarte/catalog/dto/CategoryRequestDTO.java
+++ src/main/java/com/samanthamaiaduarte/catalog/dto/CategoryRequestDTO.java
@@ -6,9 +6,8 @@ import jakarta.validation.constraints.*;
 @Schema(name = "Category information", description = "Payload for create or update a category")
 public record CategoryRequestDTO(
         @NotBlank(message = "Category name can't be empty.")
-        @Size(min = 1, max = 100, message = "Category name max size is 100 characters.")
+        @Size(min = 1, max = 100, message = "Category name size is between 1 and 100 characters.")
         String name,
 
         @Size(max = 200, message = "Category description max size is 200 characters.")
-        String description) {
-}
+        String description) { }
